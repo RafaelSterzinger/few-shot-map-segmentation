99a4231e279a42e27f8ae3c034561831d66237f7
Rafael Sterzinger <rafael.sterzinger@tuwien.ac.at>
2025-03-14 12:14:29 +0100

    implement icdar dataset

--- data/dataset.py
+++ data/dataset.py
@@ -50,12 +50,14 @@ def build_dataloader(args, transformer, use_mixup=False, use_cutmix=False):
                     if args.nshots == 10 and split != 'test' and args.seed == 42:
                         path = os.path.join('/data/databases', f'maps/maps_siegfried/dataset_{args.class_name}/fewshot10')
                         print("Using predefined dataset with 10 shots from original paper")
+                    elif split == 'val':
+                        path = os.path.join('/data/databases', f'maps/maps_siegfried/dataset_{args.class_name}/fewshot10')
                     else:
                         path = os.path.join('/data/databases', f'maps/maps_siegfried/dataset_{args.class_name}')
 
                     dataset = DatasetSiegfried(path, transformer, split, args.nshots, is_unet=True if args.base_model == 'unet' else False)
 
                 is_train = split == 'train'
-                dataloaders.append(DataLoader(dataset, batch_size=args.batch_size if is_train else args.batch_size*2, shuffle=is_train, num_workers=8, drop_last=False))#, collate_fn=collate_fn if is_train else default_collate))
+                dataloaders.append(DataLoader(dataset, batch_size=args.batch_size if is_train else args.batch_size, shuffle=is_train, num_workers=8, drop_last=False))#, collate_fn=collate_fn if is_train else default_collate))
 
         return dataloaders
---
+++ data/map_icdar.py
@@ -0,0 +1,138 @@
+from math import ceil
+import os
+from torch.utils.data import Dataset
+from patchify import patchify, unpatchify
+import torch
+import PIL.Image as Image
+import numpy as np
+import albumentations as A
+
+from data.map_siegfried import DatasetSiegfried
+
+
+class DatasetICDAR(Dataset):
+    def __init__(self, datapath, transform, split, nshots = 1.0, is_unet=False):
+        print('Loading data from %s' % datapath)
+        self.split = split
+        self.benchmark = 'maps_icdar'
+        self.base_path = datapath
+        self.transform = transform
+        self.is_unet = is_unet
+        self.images = self.load_images(self.split)
+
+
+        assert nshots == 1.0
+
+        patch_size = 448
+        if split == 'train':
+            self.augmentations = A.Compose([
+                    A.CropNonEmptyMaskIfExists(patch_size, patch_size),
+                    A.Resize(patch_size//2, patch_size//2),
+                    A.D4()
+                ])
+        else:
+            for i in range(len(self.images)):
+                orig_size = self.images[i]['orig_size']
+                transform = A.Compose([
+                    A.PadIfNeeded(min_height=patch_size*(ceil(orig_size[0]/patch_size)), min_width=patch_size*(ceil(orig_size[1]/patch_size)), border_mode=0),  
+                ])
+                new_size = None
+                patch_shape = None
+                for key,value in self.images[i].items(): 
+                    if key == 'patch_size' or key == 'orig_size' or key == 'frame_mask':
+                        continue
+                    value = transform(image=value)['image']
+                    if new_size is None:
+                        new_size = value.shape
+                    self.images[i][key] = patchify(value, (patch_size,patch_size, 3) if key == 'input' else (patch_size,patch_size), step=patch_size)
+                    shape = self.images[i][key].squeeze().shape
+                    if patch_shape is None:
+                        patch_shape = shape
+                    self.images[i][key] = self.images[i][key].squeeze().reshape(-1, *shape[2:])
+                self.images[i]['new_size'] = new_size
+                self.images[i]['patch_shape'] = patch_shape
+            self.start_indices = [0]  # Track where each list starts in global indexing
+            self.length = 0
+            for lst in self.images:
+                self.start_indices.append(self.start_indices[-1] + len(lst['input']))
+                self.length += len(lst['input'])
+
+            self.augmentations = A.Compose([
+                        A.Resize(patch_size//2, patch_size//2),
+                    ])
+
+    def global_to_local(self, global_index):
+        """Finds the correct list and local index for a given global index."""
+        for list_idx in range(len(self.images)):
+            if global_index < self.start_indices[list_idx + 1]:  
+                local_index = global_index - self.start_indices[list_idx]
+                return list_idx, local_index
+        raise IndexError("Global index out of range")
+
+    def __len__(self):
+        return 64 if self.split == 'train' else self.length
+
+    def __getitem__(self, idx):
+        image, mask, name = self.load_frame(idx)
+
+        if self.is_unet:
+            image = self.transform(image)
+        else:
+            image = self.transform(image)['pixel_values'][0]
+
+        batch = {'img': image,
+                 'mask': mask,
+                 'name': name,
+                 }
+
+        return batch
+
+    def load_images(self, split):
+        def read_metadata(split):
+            data = os.listdir(os.path.join(self.base_path, split))
+            fold_n_metadata = [d.split('/')[-1].split('.')[0] for d in data]
+            return set(map(lambda x: x[0:3], fold_n_metadata))
+
+        img_metadata = read_metadata(split)
+        print('Total (%s) images are : %d' % (self.split, len(img_metadata)))
+        images = []
+        for img in img_metadata:
+            img_dict = {}
+            base_path = os.path.join(self.base_path, split)
+            img_dict['input'] = np.array(Image.open(os.path.join(base_path, img + '-INPUT.jpg')).convert('RGB'))
+            img_dict['frame_mask'] = np.array(Image.open(os.path.join(base_path, img + '-INPUT-MASK.png')).convert('L'))
+            img_dict['gt'] = np.array(Image.open(os.path.join(base_path, img + '-OUTPUT-GT.png')).convert('L'))
+            img_dict['orig_size'] = img_dict['input'].shape
+            images.append(img_dict)
+        return images
+
+    @staticmethod
+    def convert_to_binary_mask(query_mask):
+        # Initialize a binary mask with zeros (same height and width as query_mask)
+        query_mask_binary = np.zeros(query_mask.shape[:2], dtype=np.uint8)
+
+        # Set pixels matching the drawn_class to 1
+        query_mask_binary[np.all(query_mask == [255, 255, 255], axis=-1)] = 1
+
+        return query_mask_binary
+
+    def load_frame(self, idx):
+        if self.split == 'train':
+            query_img, query_mask = self.images[0]['input'], self.images[0]['gt']
+            query_name = 'train'
+        else:
+            list_idx, idx = self.global_to_local(idx)
+            img = self.images[list_idx]
+            query_img = img['input'][idx]
+            query_mask = img['gt'][idx]
+            query_name = f'{list_idx}_{idx}'
+
+        augment = self.augmentations(image=query_img, mask=query_mask)
+        query_img, query_mask = augment['image'], augment['mask']
+
+        query_img = Image.fromarray(query_img)
+
+        query_mask = query_mask / 255
+        query_mask = torch.tensor(query_mask, dtype=torch.float)
+
+        return query_img, query_mask, query_name
